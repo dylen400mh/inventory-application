173966224edb1380db993ebbbd8c4f4854fa13c8
dylen400mh <dylenbelanger2004@gmail.com>
2024-08-07 20:06:17 -0400

    Modify pool creation to connection string

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,14 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+// module.exports = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_PUBLIC_URL,
 });
